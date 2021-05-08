4354e65cf55d06584e7b436fe6cc4375975d10e7
krysnkem <49523540+krysnkem@users.noreply.github.com>
2021-05-09 00:04:34 +0100

    second commit

--- app/src/main/java/com/example/my_fun_fact/DummyData.kt
+++ app/src/main/java/com/example/my_fun_fact/DummyData.kt
@@ -5,41 +5,66 @@ object DummyData {
         FactModel(
             "Kotlin",
             R.drawable.ic_kotlin_logo,
-            "The name comes from Kotlin Island, near St. Petersburg."
+            "Inspired by Java, it was named after Kotlin Island\n" +
+                    "in Russia. Originally called Kettusaari by the Finns\n" +
+                    "('fox island') and Ketlingen by the Swedes, (maybe\n" +
+                    "stemming from 'kettel' meaning 'cauldron'). After\n" +
+                    "Russia won control of the island in 1703 it was renamed 'Kotling' then 'Kotlin'."
         ) ,
         FactModel(
             "Java",
             R.drawable.ic_java_logo,
-            "The language was initially called Oak after an oak tree that stood outside Gosling's office."
+            "Java was designed by James Gosling while he was working at Sun\n" +
+                    "Microsystems in the early 1990s. The project was initially called 'Oak' before\n" +
+                    "a highly caffeinated brainstorming session produced the name 'Java'\n" +
+                    "(although they very nearly went with 'Silk'). Java, or Jawa in Indonesian, is the\n" +
+                    "name of a large island in Indonesia that produces strong, dark and sweet\n" +
+                    "coffee. It's name can be traced back to the Sanskrit word yavadvip, yava\n" +
+                    "meaning 'barley' and dvipa meaning 'island'. Java has been a slang term for\n" +
+                    "coffee in the United States since the 1800s."
         ),
 
         FactModel(
             "JavaScript",
             R.drawable.ic_javascript_logo,
-            "The name JavaScript came from Netscape's support of Java applets within its browser."
+
+            "Originally named Mocha, a type of fine quality\n" +
+                    "coffee, it was later renamed JavaScript, combining\n" +
+                    "Java, US slang for coffee, + 'Script, 'something that\n" +
+                    "is written' from the Latin 'scriptum', 'a set of\n" +
+                    "written words or writing'." +
+                    "The name JavaScript came from Netscape's support of Java applets within its browser."
         ),
         FactModel(
             "PHP",
             R.drawable.ic_php_logo,
-            "PHP originally stood for Personal Home Page."
+            "Originally known as Personal Home Page\n" +
+                    "Construction Kit, this was later shortened to just\n" +
+                    "PHP (an acronym for Personal Home Page). It is\n" +
+                    "now accepted as the initials for PHP: Hypertext\n" +
+                    "Preprocessor."
         ),
         FactModel(
             "Python",
             R.drawable.ic_python_logo,
-                "Why is it called python?.....When he began implementing python, Creator Guido van Rossum named Python after TV\n" +
-                        "comedy Monty Python's Flying Circus. The word\n" +
-                        "'python' comes from the ancient Greek 'Puthón',\n" +
-                        "the name of a huge serpent killed by the god\n" +
-                        "Apollo. Later adopted as a generic term for non-\n" +
-                        "poisonous snakes that constrict their prey."
+                "Why is it called python?.....Dutch programmer Guido van Rossum designed Python in 1991, naming it\n" +
+                        "after the British television comedy Monty Python's Flying Circus because he\n" +
+                        "was reading the show's scripts at the time. He wanted a name that was\n" +
+                        "\"short, unique and slightly mysterious\". The word python comes from the\n" +
+                        "ancient Greek Puthón, the name of a huge serpent killed by the god Apollo.\n" +
+                        "It has been used to refer to various large, heavy-bodied, non-venomous\n" +
+                        "snakes that constrict their prey since the early 19th century."
         ),
         FactModel(
             "Swift",
             R.drawable.ic_swift_logo,
-            "The word 'swift' means 'moving with great speed or\n" +
-                    "velocity' and can be traced back to the prehistoric\n" +
-                    "'swipt' meaning to 'move in a sweeping manner'. The\n" +
-                    "swallow-like bird became known as a swift from the 17th century and is used as the language's logo."
+            "There is no clear answer to why Apple chose the name Swift but it's\n" +
+                    "definitely not named after Taylor Swift as someone asked on Quora! My\n" +
+                    "guess is they wanted to give the impression of something fast. The word\n" +
+                    "swift means 'moving with great speed or velocity' and can be traced back to\n" +
+                    "the prehistoric swipt- meaning to 'move in a sweeping manner'. The\n" +
+                    "swallow-like bird became known as a swift from the 17th century and is used\n" +
+                    "as the programming language's logo."
         ),
         FactModel(
             "C",
