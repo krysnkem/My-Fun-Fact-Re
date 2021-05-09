3465f8ffd7dddc21018571347ef1329c3fac9a69
krysnkem <49523540+krysnkem@users.noreply.github.com>
2021-05-09 20:51:52 +0100

    fourth commit

--- app/src/main/java/com/example/my_fun_fact/DummyData.kt
+++ app/src/main/java/com/example/my_fun_fact/DummyData.kt
@@ -5,22 +5,22 @@ object DummyData {
         FactModel(
             "Kotlin",
             R.drawable.ic_kotlin_logo,
-            "Inspired by Java, it was named after Kotlin Island\n" +
-                    "in Russia. Originally called Kettusaari by the Finns\n" +
-                    "('fox island') and Ketlingen by the Swedes, (maybe\n" +
-                    "stemming from 'kettel' meaning 'cauldron'). After\n" +
+            "Inspired by Java, it was named after Kotlin Island " +
+                    "in Russia. Originally called Kettusaari by the Finns " +
+                    "('fox island') and Ketlingen by the Swedes, (maybe " +
+                    "stemming from 'kettel' meaning 'cauldron'). After " +
                     "Russia won control of the island in 1703 it was renamed 'Kotling' then 'Kotlin'."
         ) ,
         FactModel(
             "Java",
             R.drawable.ic_java_logo,
-            "Java was designed by James Gosling while he was working at Sun\n" +
-                    "Microsystems in the early 1990s. The project was initially called 'Oak' before\n" +
-                    "a highly caffeinated brainstorming session produced the name 'Java'\n" +
-                    "(although they very nearly went with 'Silk'). Java, or Jawa in Indonesian, is the\n" +
-                    "name of a large island in Indonesia that produces strong, dark and sweet\n" +
-                    "coffee. It's name can be traced back to the Sanskrit word yavadvip, yava\n" +
-                    "meaning 'barley' and dvipa meaning 'island'. Java has been a slang term for\n" +
+            "Java was designed by James Gosling while he was working at Sun " +
+                    "Microsystems in the early 1990s. The project was initially called 'Oak' before " +
+                    "a highly caffeinated brainstorming session produced the name 'Java' " +
+                    "(although they very nearly went with 'Silk'). Java, or Jawa in Indonesian, is the " +
+                    "name of a large island in Indonesia that produces strong, dark and sweet " +
+                    "coffee. It's name can be traced back to the Sanskrit word yavadvip, yava " +
+                    "meaning 'barley' and dvipa meaning 'island'. Java has been a slang term for " +
                     "coffee in the United States since the 1800s."
         ),
 
@@ -28,88 +28,88 @@ object DummyData {
             "JavaScript",
             R.drawable.ic_javascript_logo,
 
-            "Originally named Mocha, a type of fine quality\n" +
-                    "coffee, it was later renamed JavaScript, combining\n" +
-                    "Java, US slang for coffee, + 'Script, 'something that\n" +
-                    "is written' from the Latin 'scriptum', 'a set of\n" +
+            "Originally named Mocha, a type of fine quality " +
+                    "coffee, it was later renamed JavaScript, combining " +
+                    "Java, US slang for coffee, + 'Script, 'something that " +
+                    "is written' from the Latin 'scriptum', 'a set of " +
                     "written words or writing'." +
                     "The name JavaScript came from Netscape's support of Java applets within its browser."
         ),
         FactModel(
             "PHP",
             R.drawable.ic_php_logo,
-            "Originally known as Personal Home Page\n" +
-                    "Construction Kit, this was later shortened to just\n" +
-                    "PHP (an acronym for Personal Home Page). It is\n" +
-                    "now accepted as the initials for PHP: Hypertext\n" +
+            "Originally known as Personal Home Page " +
+                    "Construction Kit, this was later shortened to just " +
+                    "PHP (an acronym for Personal Home Page). It is " +
+                    "now accepted as the initials for PHP: Hypertext " +
                     "Preprocessor."
         ),
         FactModel(
             "Python",
             R.drawable.ic_python_logo,
-                "Why is it called python?.....Dutch programmer Guido van Rossum designed Python in 1991, naming it\n" +
-                        "after the British television comedy Monty Python's Flying Circus because he\n" +
-                        "was reading the show's scripts at the time. He wanted a name that was\n" +
-                        "\"short, unique and slightly mysterious\". The word python comes from the\n" +
-                        "ancient Greek Puthón, the name of a huge serpent killed by the god Apollo.\n" +
-                        "It has been used to refer to various large, heavy-bodied, non-venomous\n" +
+                "Why is it called python?.....Dutch programmer Guido van Rossum designed Python in 1991, naming it " +
+                        "after the British television comedy Monty Python's Flying Circus because he " +
+                        "was reading the show's scripts at the time. He wanted a name that was " +
+                        "\"short, unique and slightly mysterious\". The word python comes from the " +
+                        "ancient Greek Puthón, the name of a huge serpent killed by the god Apollo. " +
+                        "It has been used to refer to various large, heavy-bodied, non-venomous " +
                         "snakes that constrict their prey since the early 19th century."
         ),
         FactModel(
             "Swift",
             R.drawable.ic_swift_logo,
-            "There is no clear answer to why Apple chose the name Swift but it's\n" +
-                    "definitely not named after Taylor Swift as someone asked on Quora! My\n" +
-                    "guess is they wanted to give the impression of something fast. The word\n" +
-                    "swift means 'moving with great speed or velocity' and can be traced back to\n" +
-                    "the prehistoric swipt- meaning to 'move in a sweeping manner'. The\n" +
-                    "swallow-like bird became known as a swift from the 17th century and is used\n" +
+            "There is no clear answer to why Apple chose the name Swift but it's " +
+                    "definitely not named after Taylor Swift as someone asked on Quora! My " +
+                    "guess is they wanted to give the impression of something fast. The word " +
+                    "swift means 'moving with great speed or velocity' and can be traced back to " +
+                    "the prehistoric swipt- meaning to 'move in a sweeping manner'. The " +
+                    "swallow-like bird became known as a swift from the 17th century and is used " +
                     "as the programming language's logo."
         ),
         FactModel(
             "C",
             R.drawable.ic_c_logo,
-            "Quite simply C got its name because it was\n" +
-                    "preceded by a programming language called B. C\n" +
-                    "spawned its own children including C++ and C#. It\n" +
-                    "is the third letter in the English alphabet and was\n" +
+            "Quite simply C got its name because it was " +
+                    "preceded by a programming language called B. C " +
+                    "spawned its own children including C++ and C#. It " +
+                    "is the third letter in the English alphabet and was " +
                     "originally identical to the Greek letter 'Gamma'."
         ),
         FactModel(
             "Ruby",
             R.drawable.ic_ruby_logo,
-            "Influenced by Perl, the developer chose a\n" +
-                    "colleague's birthstone which followed it in the\n" +
-                    "monthly sequence (June is Pearl, Ruby is July).\n" +
-                    "Ruby comes from the Old French 'rubi', a 'reddish\n" +
+            "Influenced by Perl, the developer chose a " +
+                    "colleague's birthstone which followed it in the " +
+                    "monthly sequence (June is Pearl, Ruby is July). " +
+                    "Ruby comes from the Old French 'rubi', a 'reddish " +
                     "precious stone', and the Latin 'rubeus, 'red'."
         ),
         FactModel(
             "Go",
             R.drawable.ic_go_logo,
-            "One of the Google developers said the name Go,\n" +
-                    "sometime referred to as Golang, was chosen\n" +
-                    "because it was 'short and easy to type'.\n" +
-                    "The word 'go', meaning 'to travel or go somewhere'\n" +
+            "One of the Google developers said the name Go, " +
+                    "sometime referred to as Golang, was chosen " +
+                    "because it was 'short and easy to type'. " +
+                    "The word 'go', meaning 'to travel or go somewhere' " +
                     "stems from the Old High German 'gan' (to go)."
         ),
         FactModel(
             "Perl",
             R.drawable.ic_perl_logo,
-            "Initially named Pearl, the alternative spelling was\n" +
-                    "adopted as the name was already taken. It comes\n" +
-                    "from the Middle French 'perle 'meaning 'bead' or\n" +
-                    "'something valuable' and the Latin 'perna' meaning\n" +
+            "Initially named Pearl, the alternative spelling was " +
+                    "adopted as the name was already taken. It comes " +
+                    "from the Middle French 'perle 'meaning 'bead' or " +
+                    "'something valuable' and the Latin 'perna' meaning " +
                     "'leg', also a mollusc shaped like a leg of mutton."
         ),
         FactModel(
             "Scala",
             R.drawable.ic_scala_logo,
-            "Scala is a combination of the first letters of\n" +
-                    "'scalable' and 'language!. It is also the Italian word\n" +
-                    "for 'stairway', as it helps users to ascend to a\n" +
-                    "better language. The logo is also an abstraction of\n" +
-                    "a staircase or steps.\n"
+            "Scala is a combination of the first letters of " +
+                    "'scalable' and 'language!. It is also the Italian word " +
+                    "for 'stairway', as it helps users to ascend to a " +
+                    "better language. The logo is also an abstraction of " +
+                    "a staircase or steps. "
         )
 
     )
